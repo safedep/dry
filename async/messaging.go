cf316b9ff9016efcf425ce13659bad3082ed1c42
abhisek <abhisek.datta@gmail.com>
2024-03-11 09:35:17 +0530

    refactor: Async closable as an interface

--- async/messaging.go
+++ async/messaging.go
@@ -8,6 +8,10 @@ import (
 // Application defined handler function for incoming messages
 type MessageHandler func(context.Context, []byte) error
 
+type ClosableMessagingService interface {
+	Close() error
+}
+
 // Interface for a subscribed queue
 type MessagingQueueSubscription interface {
 	Unsubscribe() error
@@ -15,12 +19,15 @@ type MessagingQueueSubscription interface {
 
 // Low level messaging service interface
 type MessagingService interface {
+	ClosableMessagingService
+
 	Publish(ctx context.Context, topic string, data []byte) error
 	QueueSubscribe(topic string, queue string, callback MessageHandler) (MessagingQueueSubscription, error)
-	Close() error
 }
 
 // Async request response service interface
 type AsyncRequestResponseService interface {
+	ClosableMessagingService
+
 	Request(ctx context.Context, topic string, data []byte, timeout time.Duration) ([]byte, error)
 }

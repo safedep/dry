6c00290e719bfa01605cf2b78094106c56962b39
abhisek <abhisek.datta@gmail.com>
2024-11-28 11:21:08 +0530

    feat: Add rpc topic name from full procedure name

--- async/rpc.go
+++ async/rpc.go
@@ -8,12 +8,28 @@ func RpcTopicName(serviceName, methodName string) string {
 			return ""
 		}
 
-		if r[0] == '.' || r[0] == '/' {
-			r = r[1:]
-		}
+		r = strings.TrimPrefix(r, "/")
+		r = strings.TrimSuffix(r, "/")
 
 		return strings.ReplaceAll(r, "/", ".")
 	}
 
 	return fixer(serviceName) + "." + fixer(methodName)
+}
+
+func RpcTopicNameFromFullProcedureName(fullProcedureName string) string {
+	if len(fullProcedureName) == 0 {
+		return ""
+	}
+
+	if fullProcedureName[0] == '/' {
+		fullProcedureName = fullProcedureName[1:]
+	}
+
+	parts := strings.SplitN(fullProcedureName, "/", 2)
+	if len(parts) < 2 {
+		return ""
+	}
+
+	return RpcTopicName(parts[0], parts[1])
 }
--- async/rpc_test.go
+++ async/rpc_test.go
@@ -40,3 +40,34 @@ func TestRpcTopicName(t *testing.T) {
 		})
 	}
 }
+
+func TestRpcTopicNameFromFullProcedureName(t *testing.T) {
+	cases := []struct {
+		name              string
+		fullProcedureName string
+		expected          string
+	}{
+		{
+			name:              "simple",
+			fullProcedureName: "/service/method",
+			expected:          "service.method",
+		},
+		{
+			name:              "simple with slash",
+			fullProcedureName: "/service/method/",
+			expected:          "service.method",
+		},
+		{
+			name:              "service with multiple slash",
+			fullProcedureName: "/service/a/b/c/method",
+			expected:          "service.a.b.c.method",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			actual := RpcTopicNameFromFullProcedureName(test.fullProcedureName)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}

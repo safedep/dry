ec9ef30053718fcf8bcd8903bf09e24885d10e4c
abhisek <abhisek.datta@gmail.com>
2025-01-06 11:21:16 +0530

    chore: Move protobuf serialization functions to its own package

---
+++ api/pb/serialization_test.go
@@ -0,0 +1,112 @@
+package pb
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+
+	envoy_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromYamlToPb(t *testing.T) {
+	cases := []struct {
+		name    string
+		yamlStr string
+		pbMsg   *envoy_core_v3.Address
+		errMsg  string
+	}{
+		{
+			"Deserialize correctly",
+			`socket_address:
+  address: "1.2.3.4"
+  port_value: 1234`,
+			&envoy_core_v3.Address{
+				Address: &envoy_core_v3.Address_SocketAddress{
+					SocketAddress: &envoy_core_v3.SocketAddress{
+						Address:       "1.2.3.4",
+						PortSpecifier: &envoy_core_v3.SocketAddress_PortValue{PortValue: 1234},
+					},
+				},
+			},
+			"",
+		},
+		{
+			"Deserialize bad YAML",
+			`AAAAAAA`,
+			&envoy_core_v3.Address{},
+			"unexpected token \"AAAAAAA\"",
+		},
+		{
+			"Deserialize good YAML but incompatible with Proto Msg",
+			`A: 1`,
+			&envoy_core_v3.Address{},
+			"unknown field \"A\"",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			var obj envoy_core_v3.Address
+			err := FromYaml(strings.NewReader(test.yamlStr), &obj)
+
+			if test.errMsg != "" {
+				assert.ErrorContains(t, err, test.errMsg)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, test.pbMsg.GetSocketAddress().GetAddress(), obj.GetSocketAddress().GetAddress())
+				assert.Equal(t, test.pbMsg.GetSocketAddress().GetPortValue(), obj.GetSocketAddress().GetPortValue())
+			}
+		})
+	}
+}
+
+func TestFromPbToYAML(t *testing.T) {
+	cases := []struct {
+		name         string
+		pbMsg        *envoy_core_v3.Address
+		yamlContains string
+		errMsg       string
+	}{
+		{
+			"YAML contains address",
+			&envoy_core_v3.Address{
+				Address: &envoy_core_v3.Address_SocketAddress{
+					SocketAddress: &envoy_core_v3.SocketAddress{
+						Address:       "1.2.3.4",
+						PortSpecifier: &envoy_core_v3.SocketAddress_PortValue{PortValue: 1234},
+					},
+				},
+			},
+			"address: 1.2.3.4",
+			"",
+		},
+		{
+			"YAML does not contains address",
+			&envoy_core_v3.Address{
+				Address: &envoy_core_v3.Address_SocketAddress{},
+			},
+			"socket_address: {}",
+			"",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			buffer := bytes.Buffer{}
+			writer := bufio.NewWriter(&buffer)
+
+			err := ToYaml(writer, test.pbMsg)
+			if test.errMsg != "" {
+				assert.NotNil(t, err)
+				assert.ErrorContains(t, err, test.errMsg)
+			} else {
+				writer.Flush()
+
+				assert.Nil(t, err)
+				assert.Contains(t, buffer.String(), test.yamlContains)
+			}
+		})
+	}
+}

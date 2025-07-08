0d75716f4adcfbd089e525da579c3aef7321b393
abhisek <abhisek.datta@gmail.com>
2025-07-08 09:13:11 +0530

    Add the basic contracts for multi-tenant stream

---
+++ stream/stream.go
@@ -0,0 +1,112 @@
+// Package stream provides streaming data processing primitives.
+// The design goal is to standardize the contracts and have different implementations
+// leveraging different core infra technologies.
+package stream
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrMissingTenantID = errors.New("missing tenant ID for multi-tenant stream")
+)
+
+// Stream is a minimal description of a stream as a first class citizen.
+// Implementations can use this to create a stream, read from it, write to it,
+type Stream struct {
+	// TenantID is the identifier for the tenant that owns the stream.
+	// Used only for multi-tenant streams. Keep empty for global streams.
+	TenantID string
+
+	// A service specific namespace.
+	Namespace string
+
+	// Name of the stream.
+	Name string
+
+	// Special flags for declarative configuration of the stream specific
+	// to our platform requirements.
+	IsMultiTenant bool
+}
+
+// ID returns a unique identifier for the stream based on its properties.
+// This is used to connect to the stream and perform operations on it.
+// While individual provider may have its naming conventions, we want to
+// guarantee isolation for multi-tenant streams
+func (s Stream) ID() (string, error) {
+	if s.IsMultiTenant {
+		if s.TenantID == "" {
+			return "", ErrMissingTenantID
+		}
+
+		return s.TenantID + ":" + s.Namespace + ":" + s.Name, nil
+	} else {
+		return s.Namespace + ":" + s.Name, nil
+	}
+}
+
+type StreamAccessRole int
+
+const (
+	StreamAccessNone StreamAccessRole = iota
+	StreamAccessRead
+	StreamAccessWrite
+	StreamAccessReadWrite
+)
+
+type StreamAccessRequest struct {
+	Stream Stream
+	Access StreamAccessRole
+	Expiry time.Duration
+
+	// Other metadata such as TenantID, userID etc. goes here
+}
+
+type StreamAccess struct {
+	AccessID string
+	Token    string
+
+	// Other information for stream access goes here.
+}
+
+// StreamControlPlane is the contract for providing administrative control
+// over the underlying stream processing infrastructure.
+type StreamControlPlane interface {
+	// Stream management. Should be idempotent.
+	CreateStream(ctx context.Context, stream Stream) error
+	DeleteStream(ctx context.Context, stream Stream) error
+
+	// Access control management operations.
+	CreateStreamAccess(ctx context.Context, request StreamAccessRequest) (*StreamAccess, error)
+	DeleteStreamAccess(ctx context.Context, accessID string) error
+}
+
+type StreamSerializer[T any] interface {
+	// Serialize converts a record of type T into a byte slice.
+	Serialize(record *T) ([]byte, error)
+	// Deserialize converts a byte slice into a record of type T.
+	Deserialize(data []byte) (*T, error)
+}
+
+type StreamEntity[T any] struct {
+	Record *T
+
+	// Various metadata about the record.
+	Headers map[string]string
+}
+
+type StreamReader[T any] interface {
+	Read(ctx context.Context, offset int64, limit int) ([]*StreamEntity[T], error)
+}
+
+type StreamListener[T any] interface {
+	// Listen for new records in the stream. Limit -1 means no limit.
+	Listen(ctx context.Context, offset int64, limit int) (<-chan *StreamEntity[T], error)
+}
+
+type StreamWriter[T any] interface {
+	AppendOne(ctx context.Context, record *StreamEntity[T]) error
+	AppendMany(ctx context.Context, records []*StreamEntity[T]) error
+}
